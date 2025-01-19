24e6700bc57c4a8b8f6ad2d8c5adcba6b35afc55
catalinbadila <112496548+cbd23@users.noreply.github.com>
2025-01-19 21:48:43 +0200

    Add getComputerChoice().

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,42 @@
-<script setup>
-import Header from './components/Header.vue';
-import Main from './components/Main.vue';
+<script>
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
 
-function getRoundResult(humanChoice) {
-    console.log('You choosed ' + humanChoice)
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Main,
+  },
+  data() {
+    return {
+      computerChoice: '',
+      roundResult: '',
+    };
+  },
+  methods: {
+    getComputerChoice() {
+      let randomNumber = Math.floor(Math.random() * 3)
+
+      if (randomNumber === 0) {
+        this.computerChoice = 'rock'
+      } else if (randomNumber === 1) {
+        this.computerChoice = 'paper'
+      } else this.computerChoice = 'scissors'
+
+      return this.computerChoice
+    },
+    getRoundResult(humanChoice) {
+
+      this.getComputerChoice()
+
+      console.log('You chose ' + humanChoice)
+      console.log('Computer chose ' + this.computerChoice)
+
+      this.roundResult = ''
+      console.log(this.roundResult)
+    },
+  },
 }
 </script>
 
